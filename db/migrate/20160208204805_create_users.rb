b9c397ac4ad5cb8a7c0c036f1f0fd4799915d27f
Lee Keyser-Allen <lkeyser@alum.wpi.edu>
2016-02-09 11:06:14 -0500

    Got latest, preparing for a deploy.

--- db/migrate/20160208204805_create_users.rb
+++ db/migrate/20160208204805_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :first_name
-      t.string :last_name
+      t.string :name
       t.string :mobile_phone
+      t.string :sonos_id
+      t.string :sonos_household_id
       t.timestamps null: false
     end
   end
