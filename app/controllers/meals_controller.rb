ea2d13931b9a3dcbfbe0651c0ba13c49f4c6878f
Scott Madin <smadin@gmail.com>
2016-02-09 13:30:43 -0500

    Initial create

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,12 @@
+class MealsController < ApplicationController
+
+def index
+end
+
+def new
+end
+
+def edit
+end
+
+end
---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,15 @@
+class ShopsController < ApplicationController
+
+def index
+end
+
+def new
+end
+
+def edit
+end
+
+def show
+end
+
+end
